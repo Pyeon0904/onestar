98609f013fbb0c5b2d24e3191bc3264033fe22cb
Pyeon0904 <jjp2548@gmail.com>
2025-06-17 21:50:52 +0900

    feat: 06.AboutSection
    - heroIcon(svg)를 Icon으로 활용
    - (css) shadow-md hover:scale-105
    - (css) transition-transform duration-300
    - clsx: className={clsx('~~~', className)}

--- app/components/sections/Introduce.tsx
+++ app/components/sections/Introduce.tsx
@@ -1,8 +1,37 @@
+import AcademicCapSvg from "@/app/components/icons/AcademicCapSvg";
+import ComputerSvg from "@/app/components/icons/ComputerSvg";
+
 export default function Introduce() {
 
   return (
-    <section className='h-60 bg-red-200'>
+    <section className='mb-16'>
       <h2 className='text-3xl font-bold mb-6'>소개</h2>
+      <div className='grid grid-cols-2 gap-6'>
+        <article className='bg-gray-100 shadow-md rounded-lg p-6 hover:scale-105 transition-transform duration-300'>
+          <div className='flex'>
+            <AcademicCapSvg className='mr-2'/>
+            <h3 className='text-xl font-semibold mb-4'>
+              교육 및 활동
+            </h3>
+          </div>
+          <ul className='text-gray-800 leading-relaxed'>
+            <li>2021.10 - 2025. 이차전지 충방전 개발업무(SDI)</li>
+            <li>2021.06 웹 개발 부트캠프 수료</li>
+            <li>2021.02 경기대학교 컴퓨터공학 졸업</li>
+          </ul>
+        </article>
+        <article className='bg-gray-100 shadow-md rounded-lg p-6 hover:scale-105 transition-transform duration-300'>
+          <div className='flex'>
+            <ComputerSvg className='mr-2'/>
+            <h3 className='text-xl font-semibold mb-4'>자격증</h3>
+          </div>
+          <ul className='text-gray-800 leading-relaxed'>
+            <li>정보처리기사 자격증</li>
+            <li>SQLD 자격증</li>
+            <li>ASDP 자격증</li>
+          </ul>
+        </article>
+      </div>
     </section>
   );
 }
