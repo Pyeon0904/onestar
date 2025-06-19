7bd250f1124bc375307051952e1d4a71e9e0f4a0
Onestar <jjp2548@gmail.com>
2025-06-19 14:51:21 +0900

    feat: 08.ProjectSection

--- app/components/sections/Project.tsx
+++ app/components/sections/Project.tsx
@@ -1,9 +1,18 @@
+import Image from "next/image";
+
 export default function Project() {
 
   return (
-    <section className='h-60 bg-cyan-200'>
+    <section className=''>
       <h2 className='text-3xl font-bold mb-6'>프로젝트</h2>
+      <article>
+        <Image src="https://plus.unsplash.com/premium_photo-1749723951265-6e289d2a8113?q=80&w=600"
+               alt="프로젝트1 이미지"
+               width="600"
+               height="600"
+        />
 
+      </article>
     </section>
   );
 }
