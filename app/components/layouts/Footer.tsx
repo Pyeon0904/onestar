490e9b3e6ef769f0ad92560909709a4039aacf04
Onestar <jjp2548@gmail.com>
2025-07-03 12:09:32 +0900

    feat: 10.Footer
    - Footer 적용
    - &copy; - 앰퍼센드 copyright icon
    - {new Date().getFullYear()} - 올해 연도를 가져옴

--- app/components/layouts/Footer.tsx
+++ app/components/layouts/Footer.tsx
@@ -2,7 +2,10 @@ export default function Footer() {
 
   return (
     <>
-      <footer className='w-full h-20 bg-gray-500'>
+      <footer className='bg-gray-100 py-6'>
+        <div className='container max-w-4xl mx-auto px-6 text-center'>
+          <p>&copy; {new Date().getFullYear()} jjpyeon - All rights reserved.</p>
+        </div>
       </footer>
     </>
   );
